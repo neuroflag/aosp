506012fe6f0941aa2790935b038031270bdd1f89
Nagi <nagisong@neuroflag.com>
2023-10-27 13:54:34 +0800

    hermione: set primary screen rotation to reverse vertical

--- neuroflag_hermione_rk3568/BoardConfig.mk
+++ neuroflag_hermione_rk3568/BoardConfig.mk
@@ -1,3 +1,3 @@
 include device/rockchip/rk356x/rk3568_r/BoardConfig.mk
 PRODUCT_KERNEL_DTS := rk3568-neuroflag-hermione-lvds-boe-ev121wxm
-SF_PRIMARY_DISPLAY_ORIENTATION := 90
+SF_PRIMARY_DISPLAY_ORIENTATION := 270
